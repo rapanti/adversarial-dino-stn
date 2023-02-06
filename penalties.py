4891e8b7b7865592f9f786184f51e543ad9fa868
rapanti <ivo.rapant@uranus.uni-freiburg.de>
2023-02-06 11:44:54 +0100

    Added OverlapPenalty

--- penalties.py
+++ penalties.py
@@ -2,6 +2,7 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.functional import affine_grid
 from torchmetrics import StructuralSimilarityIndexMeasure as SSIM
 from torchvision import transforms
 from utils import grad_reverse, grad_rescale
@@ -57,6 +58,115 @@ class ThetaCropsPenalty(nn.Module):
         return self.loss_fn(det, targed) + self.loss_fn(txy, target)
 
 
+class OverlapPenalty(nn.Module):
+    """
+    Another penalty that uses theta. Generates the sampling/affine grid from theta. The grid is then used to calculate
+    the overlap between the different views. Global to global and local to local cross-similarity.
+
+    Two different approaches are implemented:
+    1. _affine_grids + _overlap1: affine_grids works as expected, overlap1 calculates then the overlap based on the
+        minima and maxima per row, column and  axes
+    2. _flatten_grids + _overlap: similar to affine grids but flattened per axes, therefore the minima and maxima
+        are calculated per axes.
+    """
+    def __init__(self, eps=1, invert=False, **kwargs):
+        super().__init__()
+        self.eps = eps
+        self.invert = invert
+
+    def forward(self, thetas, **args):
+        # create own affine grids of thetas
+        # grids = self._affine_grids(thetas) working with self._overlap1
+        grids = self._flatten_grids(thetas)
+
+        total_loss = 0
+        n_loss_terms = 0
+
+        # global views
+        global1, global2 = grids[:2]
+        # loss = self._overlap1(global1, global2) working with _affine_grids
+        loss = self._overlap(global1, global2)
+        total_loss += loss.mean()
+        n_loss_terms += 1
+
+        # local views
+        for ida, a in enumerate(grids[2:]):
+            for idb in range(ida + 3, len(grids)):
+                b = grids[idb]
+                # loss = self._overlap1(a, b) working with _affine_grids
+                loss = self._overlap(a, b)
+                total_loss += loss.mean()
+                n_loss_terms += 1
+        total_loss /= n_loss_terms
+
+        if self.invert:
+            total_loss = grad_reverse(total_loss, self.eps)
+        else:
+            total_loss = grad_rescale(total_loss, self.eps)
+
+        return total_loss
+
+    @staticmethod
+    def _affine_grids(thetas):
+        """
+        Generally affine grid produces grids with shape: [BS, W, H, 2]
+        Here 2x2 affine grids should be sufficient: [BS, 2, 2, 2]
+        """
+        grids = [affine_grid(theta, [theta.size(0), 1, 2, 2], True) for theta in thetas]
+        return grids
+
+    @staticmethod
+    def _flatten_grids(thetas):
+        """
+        Flattened and reshaped grid: [BS, 2, 4], instead of [BS, 2, 2, 2]
+        """
+        grids = []
+        for theta in thetas:
+            grid = affine_grid(theta, [theta.size(0), 1, 2, 2], True)  # grid.shape: [bs, 2, 2, 2]
+            grid = grid.reshape(grid.size(0), 2, -1)  # grid.shape: [bs, 2, 4]
+            grids.append(grid)
+        return grids
+
+    @staticmethod
+    def _overlap(grid1, grid2):
+        left1, right1 = grid1[:, 0, :].min(dim=1).values, grid1[:, 0, :].max(dim=1).values  # min/max of x-coord/axis
+        top1, bottom1 = grid1[:, 1, :].min(dim=1).values, grid1[:, 1, :].max(dim=1).values  # min/max of y-coord/axis
+        left2, right2 = grid2[:, 0, :].min(dim=1).values, grid2[:, 0, :].max(dim=1).values  # min/max of x-coord/axis
+        top2, bottom2 = grid2[:, 1, :].min(dim=1).values, grid2[:, 1, :].max(dim=1).values  # min/max of y-coord/axis
+
+        x = torch.clamp(torch.min(right1, right2) - torch.max(left1, left2), 0)  # length of x-overlap
+        y = torch.clamp(torch.min(bottom1, bottom2) - torch.max(top1, top2), 0)  # length of y-overlap
+        area = x * y / 4.
+
+        return area
+
+    @staticmethod
+    def _overlap1(grid1, grid2):
+        left1, top1 = grid1[:, :, 0, 0].min(dim=-1).values, grid1[:, 0, :, 0].min(dim=-1).values
+        right1, bottom1 = grid1[:, :, -1, 0].max(dim=-1).values, grid1[:, -1, :, 0].max(dim=-1).values
+        left2, top2 = grid2[:, :, 0, 0].min(dim=-1).values, grid2[:, 0, :, 0].min(dim=-1).values
+        right2, bottom2 = grid2[:, :, -1, 0].max(dim=-1).values, grid2[:, -1, :, 0].max(dim=-1).values
+
+        x = torch.clamp(torch.min(right1, right2) - torch.max(left1, left2), 0)
+        y = torch.clamp(torch.min(bottom1, bottom2) - torch.max(top1, top2), 0)
+        area = x * y / 4.
+
+        return area
+
+    @staticmethod
+    def _overlap2(grid1, grid2):
+        """NOT WORKING - NO GRADIENT"""
+        left1, right1 = grid1[:, 0, 0, 0], grid1[:, -1, -1, 0]
+        bottom1, top1 = grid1[:, -1, -1, 1], grid1[:, 0, 0, 1]
+        left2, right2 = grid2[:, 0, 0, 0], grid2[:, -1, -1, 0]
+        bottom2, top2 = grid2[:, -1, -1, 1], grid2[:, 0, 0, 1]
+
+        x = torch.clamp(torch.min(right1, right2) - torch.max(left1, left2), 0)
+        y = torch.clamp(torch.min(bottom1, bottom2) - torch.max(top1, top2), 0)
+
+        return x * y / 4.
+
+
 class ThetaLoss(nn.Module):
     def __init__(self, eps=1, invert=False, **kwargs):
         super().__init__()
@@ -68,7 +178,7 @@ class ThetaLoss(nn.Module):
         identity = torch.tensor([[[1, 0, 0], [0, 1, 0]]], dtype=torch.float, device=thetas[0].get_device())
         loss = 0
         for t in thetas:
-            loss = loss + self.loss_fn(t, identity)
+            loss += self.loss_fn(t, identity)
 
         loss /= len(thetas)
 
@@ -91,20 +201,20 @@ class SIMLoss(nn.Module):
 
     def forward(self, images, target, **kwargs):
         target = self.resize(torch.stack(target))
-        loss = 0
+        total_loss = 0
         for img in images:
-            step = 1 - self.loss_fn(self.resize(img), target)
-            step[step < self.min_sim] = 0
-            loss += step
+            loss = 1 - self.loss_fn(self.resize(img), target)
+            loss[loss < self.min_sim] = 0
+            total_loss += loss
 
-        loss /= len(images)
+        total_loss /= len(images)
 
         if self.invert:
-            loss = grad_reverse(loss, self.eps)
+            total_loss = grad_reverse(total_loss, self.eps)
         else:
-            loss = grad_rescale(loss, self.eps)
+            total_loss = grad_rescale(total_loss, self.eps)
 
-        return loss
+        return total_loss
 
 
 """
